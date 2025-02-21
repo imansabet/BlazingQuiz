efaa3985068ea3035f78f4a6deebe7ad082cfbbf
iman <dev.imansabet@gmail.com>
2025-02-21 12:35:48 +0330

    Web Project : Implement Login Functionality

--- BlazingQuiz.Shared/DTOs/LoginDto.cs
+++ BlazingQuiz.Shared/DTOs/LoginDto.cs
@@ -1,7 +1,11 @@
-﻿namespace BlazingQuiz.Shared.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazingQuiz.Shared.DTOs;
 
 public  class LoginDto
 {
+    [Required,EmailAddress,DataType(DataType.EmailAddress)]
     public string UserName { get; set; }
+    [Required]
     public string Password { get; set; }
 }
